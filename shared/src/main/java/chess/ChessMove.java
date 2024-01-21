d8d2ceb8a76060802aab75cec1e428323ebc85cf
jacksonbaxter <tech.xs8kl@simplelogin.com>
2024-01-20 20:19:20 -0700

    Working on ChessGame and ChessMove, implemented ChessPosition

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Objects;
+
 /**
  * Represents moving a chess piece on a chessboard
  * <p>
@@ -7,9 +9,46 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessMove {
+    private final ChessPosition start;
+    private final ChessPosition end;
+    private final ChessPiece.PieceType promotionPiece;
 
-    public ChessMove(ChessPosition startPosition, ChessPosition endPosition,
-                     ChessPiece.PieceType promotionPiece) {
+    public ChessMove(ChessPosition startPosition, ChessPosition endPosition, ChessPiece.PieceType promotionPiece) {
+        this.start = Objects.requireNonNull(startPosition);
+        this.end = Objects.requireNonNull(endPosition);
+        this.promotionPiece = promotionPiece;
+    }
+
+    /**
+     * Applies this move to the chessboard. Does not check for validity.
+     * @param board The board to make the move on.
+     */
+    public void apply(ChessBoard board) {
+        ChessPiece startPiece = board.getPiece(this.start);
+
+        // Handle special moves
+        if (isCastlingMove(startPiece)) {
+            applyCastlingMove(board, startPiece);
+        } else if (isEnPassantMove(startPiece)) {
+            applyEnPassantMove(board, startPiece);
+        } else {
+            // Standard move or pawn promotion
+            ChessPiece movedPiece = this.promotionPiece == null ? startPiece : new ChessPiece(startPiece.getTeamColor(), this.promotionPiece);
+            board.addPiece(this.end, movedPiece);
+            board.addPiece(this.start, null);
+
+            // Additional logic for rook and king moves
+            updateCastlePrivileges(board, startPiece);
+        }
+    }
+
+    private boolean isCastlingMove(ChessPiece piece) {
+        // Castling only possible with the king
+        if(piece.getPieceType() != ChessPiece.PieceType.KING) {
+            return false;
+        }
+
+        //
     }
 
     /**
