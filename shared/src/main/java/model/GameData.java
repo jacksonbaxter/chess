426b003373f713cc595f7fac794ee9337a70503b
jacksonbaxter <tech.xs8kl@simplelogin.com>
2024-03-27 17:07:21 -0600

    Fixed GameData

--- shared/src/main/java/model/GameData.java
+++ shared/src/main/java/model/GameData.java
@@ -1,4 +1,4 @@
 package model;
 
-public record GameData(int gameID, String whiteUsername, String blackUsername, String gameName, chess.ChessGame chessGame) {
+public record GameData(int gameID, String whiteUsername, String blackUsername, String gameName) {
 }
